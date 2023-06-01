4bef109ac83f3f3a245d77e89317bed867c9a46c
joseDev <jose96corrza@gmail.com>
2023-05-31 19:00:11 -0500

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 
+import { BrowserRouter, useRoutes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home';
+import MyOrders from './pages/MyOrders';
+import MyOrder from './pages/MyOrder';
+import SignIn from './pages/SignIn';
+import NotFound from './pages/NotFound';
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home />},
+    { path: '/my-orders', element: <MyOrders />},
+    { path: '/my-order', element: <MyOrder />},
+    { path: '/signin', element: <SignIn />},
+    { path: '/*', element: <NotFound />},
+  ])
+  return routes;
+}
+
+const App= () => {
 
   return (
-    <>
-      <div className='bg-cyan-300'>
-        Hola mundo !
-      </div>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
